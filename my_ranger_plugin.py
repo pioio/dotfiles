8d5c96c2de471667054f3c3d31810cfad6392281
pioio <76569454+pioio@users.noreply.github.com>
2021-07-24 17:19:37 +0200

    adding ranger files

---
+++ my_ranger_plugin.py
@@ -0,0 +1,20 @@
+# You always need to import ranger.api.commands here to get the Command class:
+from ranger.api.commands import *
+
+# My additional commands
+class terminal(Command):
+    """:terminal
+    Open new tmux split in the current directory.
+    """
+    def execute(self):
+        import os
+        from ranger.ext.get_executables import get_executables
+        if os.environ.get('TMUX'):
+            command = 'tmux split-window -v'
+        else:
+            command = os.environ.get('TERMCMD', os.environ.get('TERM'))
+            if command not in get_executables():
+                command = 'x-terminal-emulator'
+            if command not in get_executables():
+                command = 'xterm'
+        self.fm.run(command, flags='f')
